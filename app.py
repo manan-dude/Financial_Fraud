cd1143b71baa385d126d093fe139e95723654815
manan <manan9950678721@gmail.com>
2023-03-20 10:51:06 +0530

    init commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pickle
 import re
-from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 port_stem = PorterStemmer()
@@ -14,7 +13,8 @@ def stemming(content):
     con=re.sub('[^a-zA-Z]', ' ', content)
     con=con.lower()
     con=con.split()
-    con=[port_stem.stem(word) for word in con if not word in stopwords.words('english')]
+    arr = ['be', 'further', 'does', 's', 'some', 'because', 'aren', "shan't", 'most', 'i', 'had', 'about', 'shan', 'on', 'nor', "won't", "isn't", 'they', "you've", 'why', 're', 'haven', 'while', "weren't", "mustn't", 'or', 'but', 'couldn', 'didn', 'you', 'too', 'myself', 't', 'own', 'were', 'than', 'that', 'by', 'he', 'isn', 'where', "should've", "you'd", 'hers', 'same', "wasn't", 'during', "you'll", "couldn't", 'do', "didn't", 'as', 'which', 'after', 'against', 'doing', "aren't", 'can', 'of', 'have', 'hadn', 'both', 'having', 'any', 'more', 'before', 'ain', 'mustn', 'was', "wouldn't", 'mightn', 'other', 'wasn', 'through', 'if', 'no', 'o', 'hasn', 'has', 'from', 'below', 'into', 'this', 'off', 'very', "needn't", 'won', 'to', 'd', 'the', 'been', 'don', 'then', 'our', 'their', 'under', 'yours', 'whom', 'at', 'between', 'them', 'these', 'an', 'out', "mightn't", 'me', 'itself', 'who', 'down', 'will', 'themselves', 'doesn', 'for', 'weren', 'what', "that'll", 'there', 'herself', 'over', 'wouldn', "hasn't", "you're", 'with', "haven't", 'her', "she's", 'each', 'ma', 'few', 've', 'once', "shouldn't", 'his', 'your', 'until', 'and', 'above', 'him', 'yourselves', 'm', 'here', 'not', 'shouldn', 'needn', 'ourselves', 'she', 'its', 'being', 'my', 'a', 'those', 'theirs', 'am', 'so', 'should', "it's", 'ours', 'up', 'in', 'are', 'again', 'it', 'such', 'yourself', 'how', 'all', 'just', "hadn't", 'y', 'is', "don't", 'we', 'when', 'now', 'only', 'himself', "doesn't", 'll', 'did']
+    con=[port_stem.stem(word) for word in con if not word in arr]
     con=' '.join(con)
     return con
 
